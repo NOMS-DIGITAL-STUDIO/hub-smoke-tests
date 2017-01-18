47aec62ad035b0b261a7c3608dd1678451674699
Nick Redshaw <nickredshaw@gmail.com>
2017-01-18 14:47:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Hub Smoke Tests
+====
+
+This project contains the Groovy/Spock smoke tests for The Hub
+
+Ministry of Justice.
+National Offender Management Service.
+
+ 
+ 
---
+++ src/test/groovy/MongoDbSmokeTest.groovy
@@ -0,0 +1,28 @@
+import com.mongodb.BasicDBObject
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
+import com.mongodb.client.MongoDatabase
+import spock.lang.Specification
+
+import static org.assertj.core.api.Assertions.assertThat
+
+class MongoDbSmokeTest extends Specification {
+
+    def 'MongoDb health'() {
+        setup:
+        String mongoConnectionUri = System.getenv 'MONGODB_CONNECTION_URI'
+        if (!mongoConnectionUri) {
+            mongoConnectionUri = 'mongodb://localhost:27017'
+        }
+
+        MongoClient mongoClient = new MongoClient(new MongoClientURI(mongoConnectionUri))
+        MongoDatabase database = mongoClient.getDatabase 'hub_metadata'
+
+        when:
+        def command = database.runCommand(new BasicDBObject('buildInfo', 1))
+
+        then:
+        assertThat(command.getString("version")).isNotEmpty()
+    }
+
+}
