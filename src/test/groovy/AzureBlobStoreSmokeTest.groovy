15077e1a358f5805f8a7ad978a7646649ebf21c6
Nick Redshaw <nickredshaw@gmail.com>
2017-01-19 17:15:45 +0000

    Added BDD text to the tags

--- src/test/groovy/AzureBlobStoreSmokeTest.groovy
+++ src/test/groovy/AzureBlobStoreSmokeTest.groovy
@@ -12,8 +12,8 @@ class AzureBlobStoreSmokeTest extends Specification {
     private static final String AZURE_CONTAINER_NAME = "content-items"
     private static final String IMAGE_FILE_NAME = 'hub-smoke-tests-1-pixel.png'
 
-    def 'Azure blob store health'() {
-        given:
+    def 'Azure blob store is healthy'() {
+        given: 'I connect to the Azure blob store'
         String azureConnectionUri = System.getenv 'AZURE_BLOB_STORE_CONNECTION_URI'
         if (!azureConnectionUri) {
             fail 'AZURE_BLOB_STORE_CONNECTION_URI environment variable was not set'
@@ -27,14 +27,14 @@ class AzureBlobStoreSmokeTest extends Specification {
         CloudBlobClient blobClient = CloudStorageAccount.parse(azureConnectionUri).createCloudBlobClient()
         CloudBlobContainer container = blobClient.getContainerReference(AZURE_CONTAINER_NAME)
 
-        when:
+        when: 'I upload a file and then access its public URL'
         File file = new File(getClass().getResource(IMAGE_FILE_NAME).toURI())
         container.getBlockBlobReference(IMAGE_FILE_NAME).upload(new FileInputStream(file), file.size())
 
         String uri = format("%s/%s/%s", azurePublicUrlBase, AZURE_CONTAINER_NAME, IMAGE_FILE_NAME)
         HttpResponse<String> imageResponse = Unirest.get(uri).asString()
 
-        then:
+        then: 'I get a HTTP status OK response'
         imageResponse.getStatus() == 200
     }
 
--- src/test/groovy/MongoDbSmokeTest.groovy
+++ src/test/groovy/MongoDbSmokeTest.groovy
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.fail
 
 class MongoDbSmokeTest extends Specification {
 
-    def 'MongoDb health'() {
-        given:
+    def 'MongoDb is healthy'() {
+        given: 'I connect to the MongoDB Media Store'
         String mongoConnectionUri = System.getenv 'MONGODB_CONNECTION_URI'
         if (!mongoConnectionUri) {
             fail 'MONGODB_CONNECTION_URI environment variable was not set'
@@ -18,10 +18,10 @@ class MongoDbSmokeTest extends Specification {
         MongoClient mongoClient = new MongoClient(new MongoClientURI(mongoConnectionUri))
         MongoDatabase database = mongoClient.getDatabase 'hub_metadata'
 
-        when:
+        when: 'I run the buildInfo command'
         def command = database.runCommand(new BasicDBObject('buildInfo', 1))
 
-        then:
+        then: 'the version string is present in the response object'
         command.getString('version').length() != 0
     }
 
