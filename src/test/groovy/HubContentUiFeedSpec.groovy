9e74839aa1ef2c994d5eb3fe8ddeb58707813bf4
Nick Redshaw <nickredshaw@gmail.com>
2017-02-23 16:46:59 +0000

    HUB-193: Ignore content feed ui test until the app is ready

--- src/test/groovy/HubContentUiFeedSpec.groovy
+++ src/test/groovy/HubContentUiFeedSpec.groovy
@@ -1,10 +1,12 @@
 import com.mashape.unirest.http.HttpResponse
 import com.mashape.unirest.http.Unirest
 import org.apache.http.HttpStatus
+import spock.lang.Ignore
 import spock.lang.Specification
 
 class HubContentUiFeedSpec extends Specification {
 
+    @Ignore
     def 'Content Feed UI is healthy'() {
         def hubContentFeedUriUrI = (System.getenv('HUB_CONTENT_FEED_UI_URI') ?: "http://localhost:8000/")
 
