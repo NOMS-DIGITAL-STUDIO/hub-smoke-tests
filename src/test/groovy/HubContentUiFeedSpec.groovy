3a6b02595d55942b0feecb082f8a9fb5e70bc946
Nick Redshaw <nickredshaw@gmail.com>
2017-02-23 16:40:28 +0000

    HUB-193: Rename the env var

--- src/test/groovy/HubContentUiFeedSpec.groovy
+++ src/test/groovy/HubContentUiFeedSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 class HubContentUiFeedSpec extends Specification {
 
     def 'Content Feed UI is healthy'() {
-        def hubContentFeedUriUrI = (System.getenv('HUB_CONTENT_FEED_URI') ?: "http://localhost:8000/")
+        def hubContentFeedUriUrI = (System.getenv('HUB_CONTENT_FEED_UI_URI') ?: "http://localhost:8000/")
 
         when: 'I access the Content Feed UI'
         HttpResponse<String> response = Unirest.get(hubContentFeedUriUrI).asString();
