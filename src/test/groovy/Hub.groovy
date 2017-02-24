3093311619d66915c0f868d9f676c8d51f516188
Nick Redshaw <nickredshaw@gmail.com>
2017-02-24 11:47:53 +0000

    HUB-000: Remove basic auth from Content Feed uri

--- src/test/groovy/Hub.groovy
+++ src/test/groovy/Hub.groovy
@@ -19,7 +19,7 @@ class Hub {
 
         contentFeedUri = (System.getenv('HUB_CONTENT_FEED_URI') ?: "http://localhost:8080/hub-content-feed/")
         log.info("contentFeedUri: ${contentFeedUri}")
-        contentFeedUri = contentFeedUri.replaceFirst('^https?://', "http://${username}:${password}@")
+        println "contentFeedUri: ${contentFeedUri}"
 
         adminUiUri = (System.getenv('HUB_ADMIN_UI_URI') ?: "http://localhost:3000/")
         log.info("adminUiUri: ${adminUiUri}")
